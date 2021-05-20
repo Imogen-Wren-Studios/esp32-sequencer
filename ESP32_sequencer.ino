28a8ef543eb608af5b0ce7547f4131907013bb50
PanGalacticTech <53580358+PanGalacticTech@users.noreply.github.com>
2021-05-20 23:51:54 +0100

    Initial commit

---
+++ ESP32_sequencer.ino
@@ -0,0 +1,49 @@
+/*  ESP32 |1| |2| |3| |4| |5| |6| |7| |8| Step Sequencer 
+ * 
+ * Step sequencer with 8 beats
+ * 
+ *  Inputs:
+ *    - Tap Tempo
+ *    - Rotary Set Tempo
+ *    - CH1 variable Per Step
+ *    - CH2 Variable Per Step
+ *    - Midi Clock
+ *    - Groove
+ *  
+ *  
+ *  Outputs: 
+ *    - CH1 Analog Voltage Per Step
+ *    - CH2 Analog Voltage Per Step
+ *    - LED Per Step
+ *    - MDI Clock
+ *    - LED Tempo Heartbeat
+ *    - Clock/Tempo Value
+ *  
+ * 
+ * 
+ */
+
+
+
+
+
+
+void setup() {
+
+
+
+  
+}
+
+
+byte current_step = 0;
+
+uint16_t tempo = 120;
+
+
+
+void loop() {
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ESP32_sequencer
+ 8 Step Sequencer for Eurorack style synth control
